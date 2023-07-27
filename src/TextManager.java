b2b2c68782970df507d45bfb86ef58dff0da6094
noahr <noahr@DESKTOP-0x69.lan>
2023-07-27 00:33:38 -0500

    Revert

--- src/TextManager.java
+++ src/TextManager.java
@@ -6,16 +6,6 @@ public class TextManager {
 		this.textPaneContents = "";
 	}
 	
-<<<<<<< HEAD
-	public void setTextPaneContents(String arg) { 
-		this.textPaneContents = "";
-		this.textPaneContents = arg; 
-	}
-=======
-	public void setTextPaneContents(String arg) { this.textPaneContents = arg; 
-	String test = "poop";
-	}
-
->>>>>>> branch 'master' of https://github.com/noahratzburg/GCodeTextEditor.git
+	public void setTextPaneContents(String arg) { this.textPaneContents = arg; }
 	public String getTextPaneContents() { return this.textPaneContents; }
 }
