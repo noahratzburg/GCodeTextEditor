b3c8eb0b14c7c220de5f5bc62d7790d675229945
noahr <noahr@DESKTOP-0x69.lan>
2023-07-26 22:30:16 -0500

    Added Document Listener

--- src/Editor.java
+++ src/Editor.java
@@ -1,63 +1,7 @@
-//
-//
-//import java.awt.Dimension;
-//import java.awt.FlowLayout;
-//import java.awt.Font;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.util.Scanner;
-//
-//import javax.swing.JFrame;
-//import javax.swing.JScrollPane;
-//import javax.swing.JTextArea;
-//import javax.swing.ScrollPaneConstants;
-//
-//public class Editor extends JFrame implements ActionListener{
-//
-//	JTextArea textArea;
-//	JScrollPane scrollPane;
-//	
-//	String pgm;
-//	String[] items;
-//	String[] colorCode;
-//	
-//	Scanner scanner = new Scanner(pgm);
-//	
-//	
-//	
-//	Editor(){
-//		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		this.setTitle("Gcode Translate");
-//		this.setSize(500, 500);
-//		this.setLayout(new FlowLayout());
-//		this.setLocationRelativeTo(null);
-//		
-//		textArea = new JTextArea();
-//		textArea.setLineWrap(false);
-//		textArea.setFont(new Font("Arial", Font.PLAIN, 14));
-//		
-//		scrollPane = new JScrollPane(textArea);
-//		scrollPane.setPreferredSize(new Dimension(450, 450));
-//		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-//		
-//		
-//		this.add(scrollPane);
-//		this.setVisible(true);
-//		
-//	
-//	}
-//	
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		// TODO Auto-generated method stub
-//		System.out.println(e.hashCode());
-//		
-//		
-//	}
-//
-//}
-
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -86,6 +30,35 @@ public class Editor extends JFrame implements ActionListener {
 			public void keyTyped(KeyEvent e) {}
 			public void keyReleased(KeyEvent e) {}
 			
+		});
+		textPane.getDocument().addDocumentListener(new DocumentListener() {
+			public void insertUpdate(DocumentEvent e) {
+				try {
+					System.out.println("Insert...");
+					System.out.println(e.getDocument().getText(0, e.getDocument().getLength()));
+				} catch (BadLocationException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+			public void removeUpdate(DocumentEvent e) {
+				try {
+					System.out.println("Removed...");
+					System.out.println(e.getDocument().getText(0, e.getDocument().getLength()));
+				} catch (BadLocationException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+			public void changedUpdate(DocumentEvent e) {
+				try {
+					System.out.println("Changed...");
+					System.out.println(e.getDocument().getText(0, e.getDocument().getLength()));
+				} catch (BadLocationException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
 		});
 		//textArea.setLineWrap(false);
 		textPane.setFont(new Font("Arial", Font.PLAIN, 14));
