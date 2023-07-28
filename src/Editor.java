673aafbf13853d8867f6472f950ae0afbe2e53a2
Gilip <Gilip@DESKTOP-CFSG8KO>
2023-07-28 16:56:09 -0500

    test

--- src/Editor.java
+++ src/Editor.java
@@ -76,7 +76,7 @@ public class Editor extends JFrame implements ActionListener {
 		this.add(scrollPane);
 		this.setVisible(true);
 		
-		Line line = new Line("G54G90G00 X-69Y0.000009Z4.20 M8 S34 (THIS IS A test, hello)");
+		Line line = new Line("G54G90G00 X-69Y0.0000     Z4.20M8S34(THIS IS A test, hello)");
 	}
 	
 	public void actionPerformed(ActionEvent e) {
